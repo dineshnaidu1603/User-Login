b7db3b77120abd9fc7004b59e6b931f042bdc4b8
dineshnaidu1603 <dineshnaidunarni@gmail.com>
2024-02-14 23:05:38 +0530

    Added comments

--- src/main/java/cred/data.java
+++ src/main/java/cred/data.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
+// This class is used to connect to the database
 public class data {
     Connection con;
     data() throws SQLException, ClassNotFoundException {
